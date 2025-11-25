ecbf07b8a783062b69cfc1f443e6be2ad2191d9a
hurmaao <7n7f2bdymj@privaterelay.appleid.com>
2025-11-25 17:16:04 +0300

    exclude the case of prime numbers

--- kasiski_len_key.py
+++ kasiski_len_key.py
@@ -3,7 +3,7 @@ from math import gcd
 
 def kasiski(text):
     distances = []
-    for size in (4, 5):
+    for size in (3, 4, 5):
         for i in range(len(text) - size):
             sub = text[i : i + size]
             for j in range(i + size, len(text) - size):
@@ -13,9 +13,16 @@ def kasiski(text):
     if not distances:
         return None
 
-    g = distances[0]
+    if min(distances) != 1:
+        g = min(distances)
+    else:
+        dist_i = [x for x in distances if x != 1]
+        g = min(dist_i)
+
     for num in distances:
-        g = gcd(num, g)
+        if gcd(num, g) <= 9 and gcd(num, g) != 1:
+            g = gcd(num, g)
     return g
 
-#find key lenghts from compute gcd of distance between repeated letter combinations from 4 letters to avoid random coincidences
+
+# find key lenghts from compute gcd of distance between repeated letter combinations from 4 letters to avoid random coincidences
